ae536332d819114384f7490f055846ed50775f9c
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-01-18 21:49:22 -0800

    Prompt user for canvas size

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const frame = document.querySelector(".frame");
-
-for (i=0; i < 16; i++) {
+let x = prompt("Size of canvas?");
+if (x>100) {
+    x = 100;
+}
+for (i=0; i < x; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
-    for (j=0; j < 16; j++) {
+    for (j=0; j < x; j++) {
         let pix = document.createElement("div");
         pix.classList.add("pix");
         pix.addEventListener('mouseover', () => pix.style.backgroundColor = "black")
