c1e5ff4634cc9cfe9e3f254909b9bcbb917396ff
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-01-18 21:57:14 -0800

    Draw random color on mouse down, none on pure hover

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const frame = document.querySelector(".frame");
 let x = prompt("Size of canvas?");
+
+
+window.mouseDown = false;
+document.onmousedown = function() {
+    window.mouseDown = true;
+}
+document.onmouseup = function() {
+    window.mouseDown = false;
+}
+
 if (x>100) {
     x = 100;
 }
@@ -9,10 +19,17 @@ for (i=0; i < x; i++) {
     for (j=0; j < x; j++) {
         let pix = document.createElement("div");
         pix.classList.add("pix");
-        pix.addEventListener('mouseover', () => pix.style.backgroundColor = "black")
+        pix.addEventListener('mouseover', () => changeColor(pix))
         row.appendChild(pix);
     }
 
     frame.appendChild(row);
+
+}
+function changeColor(pix) {
+   // pix.style.backgroundColor = "black";
+    if (window.mouseDown) {
+        pix.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);;
+    }
 
 }
