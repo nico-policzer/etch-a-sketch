049739e2eac9e4d9abc3f23c692c31ec70f38490
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-01-18 19:06:44 -0800

    Laid out div grid

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
-
 const frame = document.querySelector(".frame");
 
 for (i=0; i < 16; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
-    for (j=0; i < 16; j++) {
+    for (j=0; j < 16; j++) {
         // add div to column
         let pix = document.createElement("div");
         pix.classList.add("pix");
+        row.appendChild(pix);
     }
 
-    // go to new row
+    frame.appendChild(row);
+
 }
