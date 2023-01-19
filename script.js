bb2974fd9c750d328b29afb7e2cca9e26f882412
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-01-18 18:50:35 -0800

    Write boilerplate HTML, skeleton of script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const frame = document.querySelector(".frame");
+
+for (i=0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    for (j=0; i < 16; j++) {
+        // add div to column
+        let pix = document.createElement("div");
+        pix.classList.add("pix");
+    }
+
+    // go to new row
+}
