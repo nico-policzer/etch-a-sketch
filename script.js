9b7c46ae3a3e0d157f86cb404542b61b372caca0
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-01-25 20:29:53 -0800

    Update disabled state of button on numeric input, add functional size change

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const canvas = document.querySelector(".canvas");
-let x = prompt("Size of canvas?");
 let color = "#" + Math.floor(Math.random()*16777215).toString(16);
 
+let button = document.querySelector("#submit");
+button.addEventListener("click", ()=> renderCanvas(getSize()));
+button.disabled = true;
+
+let sizeInput = document.querySelector("#textinput");
+sizeInput.addEventListener('change', () => updateButton());
 
 window.mouseDown = false;
-document.onmousedown = function() {
-    window.mouseDown = true;
-}
-document.onmouseup = function() {
-    window.mouseDown = false;
-}
 
-
+// Renders an x by x canvas on canvas, if x is greater than 100, a 100 x 100 canvas will be rendered
 function renderCanvas(x) {
     clearCanvas();
     if (x>100) {
@@ -30,20 +29,48 @@ function renderCanvas(x) {
     }    
 }
 
-renderCanvas(x);
+
+document.onmousedown = function() {
+    window.mouseDown = true;
+}
+document.onmouseup = function() {
+    window.mouseDown = false;
+}
 
 function clearCanvas() {
     while(canvas.firstChild) {
         canvas.removeChild(canvas.lastChild);
     }
 }
-function getColor() {
-    return document.querySelector("input").value;
+
+// If the input is not a number, the button is disabled
+function updateButton() {
+    if (isNumeric(sizeInput.value)) button.disabled = false;
+    else {
+        button.disabled = true;
+    }
 }
+
+function getSize() {
+    return sizeInput.value;
+}
+function getColor() {
+    return document.querySelector("#colorpicker").value;
+}
+
 function changeColor(pix) {
-   // pix.style.backgroundColor = "black";
     if (window.mouseDown) {
-        pix.style.backgroundColor = getColor();;
+        pix.style.backgroundColor = getColor();
     }
 
 }
+
+// returns true if the str is numeric
+function isNumeric(str) {
+    // SOURCE: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+    if (typeof str != "string") return false; // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
+  }
+
+  renderCanvas(50);
